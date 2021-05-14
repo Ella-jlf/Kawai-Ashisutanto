1d4de9a94ba7e607fa2692c5ea7d12eb7516f121
ALLLA <mini_wariors1@mail.ru>
2021-05-14 04:22:29 +0300

    Bottom Menu Created

--- app/src/main/java/android/ella/assistant/MainActivity.kt
+++ app/src/main/java/android/ella/assistant/MainActivity.kt
@@ -1,11 +1,34 @@
 package android.ella.assistant
 
+import android.ella.assistant.databinding.ActivityMainBinding
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mBinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        initBottomNavigationView()
+
+
+
+        
     }
+
+
+    private fun initBottomNavigationView(){
+        val navControl = findNavController(R.id.fragment_container)
+        mBinding.bottomNavigationView.setupWithNavController(navControl)
+    }
+
+
+
+
 }
---
+++ app/src/main/java/android/ella/assistant/fragments/ListViewModel.kt
@@ -0,0 +1,7 @@
+package android.ella.assistant.fragments
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
